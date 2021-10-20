fa9d2a41f340a4e63ee4c01e7be0dfe3c679baad
Ravadasatish <92287840+Ravadasatish@users.noreply.github.com>
2021-10-20 20:24:12 +0700

    Add files via upload

---
+++ drive program 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+ //declaration
+ int age;
+ float cgpa;
+ char initial;
+ //initialisation
+ initial='Y';
+ age=16;
+ cgpa=9.0;
+ printf("initial of student %c\nage-%d\nCGPA-%.2f",initial,age,cgpa);	
+}
