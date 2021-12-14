894ba2df6ce88be2475600b6cfd82be341ac069c
Ravadasatish <92287840+Ravadasatish@users.noreply.github.com>
2021-12-14 09:41:29 +0530

    Add files via upload

---
+++ numbermultiple.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter your number ");
+	scanf("%d",&num);
+	if(num%1==0)
+	{
+		printf("%d is divisible by 1\n",num);
+	}
+	
+	if(num%2==0)
+	{
+		printf("%d is divisible by 2\n",num);
+	}
+	
+	if(num%3==0)
+	{
+		printf("%d is divisible by 3\n",num);
+	}
+	
+	if(num%4==0)
+	{
+		printf("%d is divisible by 4\n",num);
+	}
+	
+	if(num%5==0)
+	{
+		printf("%d is divisible by 5\n",num);
+	}
+	
+	if(num%6==0)
+	{
+		printf("%d is divisible by 6\n",num);
+	}
+	
+	if(num%7==0)
+	{
+		printf("%d is divisible by 7\n",num);
+	}
+	
+	if(num%8==0)
+	{
+		printf("%d is divisible by 8\n",num);
+	}
+	
+	if(num%9==0)
+	{
+		printf("%d is divisible by 9\n",num);
+	}
+}
